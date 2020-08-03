7c88fb41ad1e2ad3cecc66162070388a2ad79c23
30AndersonLucas <64329503+30AndersonLucas@users.noreply.github.com>
2020-08-03 17:15:04 -0300

    Commit da Segunda Aula

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import CadastroVideo from './Pages/Cadastro/Video/Index';
+import CadastroCategoria from './Pages/Cadastro/Categoria';
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   
+   <BrowserRouter>
+     <Switch>
+     < Route path= "/cadastro/video" component={CadastroVideo} />
+     < Route path= "/cadastro/categoria" component={CadastroCategoria} />
+     < Route path= "/" component={Home} exact />
+     < Route component={() => (<div> Página 404</div>)} />
+
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
