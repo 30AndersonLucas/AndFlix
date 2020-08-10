2a7a6413ec98324a07749285483fa77f6dfa3139
30AndersonLucas <64329503+30AndersonLucas@users.noreply.github.com>
2020-08-10 00:09:14 -0300

    commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+       
       </a>
       <p>
-        Orgulhosamente criado durante a
+        
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          
         </a>
       </p>
     </FooterBase>
